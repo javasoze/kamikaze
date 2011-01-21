5498046b1d80ea9b1c623cd06127337d94e97373
Hao Yan <hyan2008@gmail.com>
2011-01-21 14:36:53 -0800

    changed maven-related files

--- bin/mvn-deploy.sh
+++ bin/mvn-deploy.sh
@@ -1,1 +1,1 @@
-mvn deploy:deploy-file -Dfile=dist/kamikaze-2.0.0-SNAPSHOT.jar -DpomFile=pom.xml -Durl=http://oss.sonatype.org/content/repositories/snapshots -DrepositoryId=oss
+mvn deploy:deploy-file -Dfile=dist/kamikaze-3.0.3-SNAPSHOT.jar -DpomFile=pom.xml -Durl=https://oss.sonatype.org/content/repositories/snapshots/ -DrepositoryId=ossKamikaze
